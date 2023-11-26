4919ad8962424991e822893d22652666ff3294cc
claroja <63183535@qq.com>
2023-11-26 15:02:41 +0000

    Deploying to blog from @ claroja/claroja.github.io@669ac45f56e9f1242d4d050499ce93e0ad55a3a7 🚀

--- assets/python___call__.html-RUfvMHEc.js
+++
@@ -1,8 +0,0 @@
-import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as a,d as e}from"./app-j3zK2x6_.js";const t={},c=e(`<h1 id="call" tabindex="-1"><a class="header-anchor" href="#call" aria-hidden="true">#</a> <strong>call</strong></h1><p><code>__call__()</code>当对象当做函数被调用时触发, 一般用来归纳对象的操作步骤, 方便调用.</p><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code><span class="token keyword">class</span> <span class="token class-name">Person</span><span class="token punctuation">:</span>
-    <span class="token keyword">def</span> <span class="token function">__call__</span><span class="token punctuation">(</span>self<span class="token punctuation">)</span><span class="token punctuation">:</span>
-        <span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&quot;hello&quot;</span><span class="token punctuation">)</span>
-
-
-wang <span class="token operator">=</span> Person<span class="token punctuation">(</span><span class="token punctuation">)</span>  <span class="token comment"># 创建实例</span>
-wang<span class="token punctuation">(</span><span class="token punctuation">)</span>  <span class="token comment"># 调用实例的__call__()方法g</span>
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),o=[c];function l(p,i){return s(),a("div",null,o)}const u=n(t,[["render",l],["__file","python___call__.html.vue"]]);export{u as default};
