62d28a5f6c6c5035c103f22a2c3062701dbb82f9
claroja <63183535@qq.com>
2023-11-30 13:13:02 +0000

    Deploying to blog from @ claroja/claroja.github.io@6c9ab6136b97b204d33e1dc8722ceb873d89c33f 🚀

---
+++ assets/exception.html-U70sqIvH.js
@@ -0,0 +1,82 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as a,e}from"./app-0JgdiRQ-.js";const t={},i=e(`<h1 id="exception" tabindex="-1"><a class="header-anchor" href="#exception" aria-hidden="true">#</a> exception</h1><h2 id="raising-an-exception" tabindex="-1"><a class="header-anchor" href="#raising-an-exception" aria-hidden="true">#</a> Raising an Exception</h2><p>使用<code>raise</code>来抛出一个<code>Exception</code></p><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code>x <span class="token operator">=</span> <span class="token number">10</span>
+<span class="token keyword">if</span> x <span class="token operator">&gt;</span> <span class="token number">5</span><span class="token punctuation">:</span>
+    <span class="token keyword">raise</span> Exception<span class="token punctuation">(</span><span class="token string">&#39;x should not exceed 5. The value of x was: {}&#39;</span><span class="token punctuation">.</span><span class="token builtin">format</span><span class="token punctuation">(</span>x<span class="token punctuation">)</span><span class="token punctuation">)</span>
+
+
+<span class="token comment">## Traceback (most recent call last):</span>
+<span class="token comment">##   File &quot;&lt;input&gt;&quot;, line 4, in &lt;module&gt;</span>
+<span class="token comment">## Exception: x should not exceed 5. The value of x was: 10</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="the-assertionerror-exception" tabindex="-1"><a class="header-anchor" href="#the-assertionerror-exception" aria-hidden="true">#</a> The AssertionError Exception</h2><p>如果<code>assert</code>后面是<code>True</code>, 则程序会继续运行. 如果是<code>False</code> 则会抛出<code>AssertionError</code></p><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code><span class="token keyword">import</span> sys
+<span class="token keyword">assert</span> <span class="token punctuation">(</span><span class="token string">&#39;linux&#39;</span> <span class="token keyword">in</span> sys<span class="token punctuation">.</span>platform<span class="token punctuation">)</span><span class="token punctuation">,</span> <span class="token string">&quot;This code runs on Linux only.&quot;</span> <span class="token comment"># 如果在linux会正常运行, 如果在windows上会报错</span>
+
+<span class="token comment">## Traceback (most recent call last):</span>
+<span class="token comment">##   File &quot;&lt;input&gt;&quot;, line 2, in &lt;module&gt;</span>
+<span class="token comment">## AssertionError: This code runs on Linux only.</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="the-try-and-except-block-handling-exceptions" tabindex="-1"><a class="header-anchor" href="#the-try-and-except-block-handling-exceptions" aria-hidden="true">#</a> The try and except Block: Handling Exceptions</h2><p><code>try</code>和<code>except</code>是用来捕获和操作<code>Exception</code></p><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code><span class="token keyword">def</span> <span class="token function">linux_interaction</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">:</span>
+    <span class="token keyword">assert</span> <span class="token punctuation">(</span><span class="token string">&#39;linux&#39;</span> <span class="token keyword">in</span> sys<span class="token punctuation">.</span>platform<span class="token punctuation">)</span><span class="token punctuation">,</span> <span class="token string">&quot;Function can only run on Linux systems.&quot;</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&#39;Doing something.&#39;</span><span class="token punctuation">)</span>
+
+
+<span class="token comment">## 捕获方法异常</span>
+<span class="token keyword">try</span><span class="token punctuation">:</span>
+    linux_interaction<span class="token punctuation">(</span><span class="token punctuation">)</span>
+<span class="token keyword">except</span> AssertionError <span class="token keyword">as</span> error<span class="token punctuation">:</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span>error<span class="token punctuation">)</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&#39;The linux_interaction() function was not executed&#39;</span><span class="token punctuation">)</span>
+
+<span class="token comment">## 如果在windows上运行, 会得到如下的信息</span>
+<span class="token comment">## Function can only run on Linux systems.  # assert 输出</span>
+<span class="token comment">## The linux_interaction() function was not executed  # except 输出</span>
+
+<span class="token comment">## 捕获多个异常</span>
+<span class="token keyword">try</span><span class="token punctuation">:</span>  
+    linux_interaction<span class="token punctuation">(</span><span class="token punctuation">)</span>
+    <span class="token keyword">with</span> <span class="token builtin">open</span><span class="token punctuation">(</span><span class="token string">&#39;file.log&#39;</span><span class="token punctuation">)</span> <span class="token keyword">as</span> <span class="token builtin">file</span><span class="token punctuation">:</span>
+        read_data <span class="token operator">=</span> <span class="token builtin">file</span><span class="token punctuation">.</span>read<span class="token punctuation">(</span><span class="token punctuation">)</span>
+<span class="token keyword">except</span> FileNotFoundError <span class="token keyword">as</span> fnf_error<span class="token punctuation">:</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span>fnf_error<span class="token punctuation">)</span>
+<span class="token keyword">except</span> AssertionError <span class="token keyword">as</span> error<span class="token punctuation">:</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span>error<span class="token punctuation">)</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&#39;Linux linux_interaction() function was not executed&#39;</span><span class="token punctuation">)</span>
+
+<span class="token comment">## 如果在windows上运行, 会得到和\`捕获方法异常\`一样的记过</span>
+<span class="token comment">## 如果在linux上运行, 则会得到\`[Errno 2] No such file or directory: &#39;file.log&#39;\`</span>
+<span class="token comment">## 1. try里面的代码, 会在遇到第一个exception时停止</span>
+<span class="token comment">## 2. 在except中, 决定如果处理try中遇到的exception</span>
+<span class="token comment">## 3. 不建议使用\`except Exception as error\`, 可在https://docs.python.org/3/library/exceptions.html查看具体的exception</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="the-else-clause" tabindex="-1"><a class="header-anchor" href="#the-else-clause" aria-hidden="true">#</a> The else Clause</h2><p><code>else</code>是在<code>except</code>没有执行的话时, 执行</p><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code><span class="token keyword">def</span> <span class="token function">linux_interaction</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">:</span>
+    <span class="token keyword">assert</span> <span class="token punctuation">(</span><span class="token string">&#39;linux&#39;</span> <span class="token keyword">in</span> sys<span class="token punctuation">.</span>platform<span class="token punctuation">)</span><span class="token punctuation">,</span> <span class="token string">&quot;Function can only run on Linux systems.&quot;</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&#39;Doing something.&#39;</span><span class="token punctuation">)</span>
+
+<span class="token keyword">try</span><span class="token punctuation">:</span>
+    linux_interaction<span class="token punctuation">(</span><span class="token punctuation">)</span>
+<span class="token keyword">except</span> AssertionError <span class="token keyword">as</span> error<span class="token punctuation">:</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span>error<span class="token punctuation">)</span>
+<span class="token keyword">else</span><span class="token punctuation">:</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&#39;Executing the else clause.&#39;</span><span class="token punctuation">)</span>
+
+<span class="token comment">## 如果在linux上运行会得到</span>
+<span class="token comment">## Doing something.</span>
+<span class="token comment">## Executing the else clause.</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="cleaning-up-after-using-finally" tabindex="-1"><a class="header-anchor" href="#cleaning-up-after-using-finally" aria-hidden="true">#</a> Cleaning Up After Using finally</h2><p><code>finally</code>是无论如何都会执行的</p><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code><span class="token keyword">try</span><span class="token punctuation">:</span>
+    linux_interaction<span class="token punctuation">(</span><span class="token punctuation">)</span>
+<span class="token keyword">except</span> AssertionError <span class="token keyword">as</span> error<span class="token punctuation">:</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span>error<span class="token punctuation">)</span>
+<span class="token keyword">else</span><span class="token punctuation">:</span>
+    <span class="token keyword">try</span><span class="token punctuation">:</span>
+        <span class="token keyword">with</span> <span class="token builtin">open</span><span class="token punctuation">(</span><span class="token string">&#39;file.log&#39;</span><span class="token punctuation">)</span> <span class="token keyword">as</span> <span class="token builtin">file</span><span class="token punctuation">:</span>
+            read_data <span class="token operator">=</span> <span class="token builtin">file</span><span class="token punctuation">.</span>read<span class="token punctuation">(</span><span class="token punctuation">)</span>
+    <span class="token keyword">except</span> FileNotFoundError <span class="token keyword">as</span> fnf_error<span class="token punctuation">:</span>
+        <span class="token keyword">print</span><span class="token punctuation">(</span>fnf_error<span class="token punctuation">)</span>
+<span class="token keyword">finally</span><span class="token punctuation">:</span>
+    <span class="token keyword">print</span><span class="token punctuation">(</span><span class="token string">&#39;Cleaning up, irrespective of any exceptions.&#39;</span><span class="token punctuation">)</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="总结" tabindex="-1"><a class="header-anchor" href="#总结" aria-hidden="true">#</a> 总结</h2><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code><span class="token keyword">try</span><span class="token punctuation">:</span>
+    <span class="token string">&quot;run this code&quot;</span>
+<span class="token keyword">except</span><span class="token punctuation">:</span>
+    <span class="token string">&quot;Execute this code when there is an exception&quot;</span>
+<span class="token keyword">else</span><span class="token punctuation">:</span>
+    <span class="token string">&quot;No exceptions? Run this code&quot;</span>
+<span class="token keyword">finally</span><span class="token punctuation">:</span>
+    <span class="token string">&quot;Always run this code.&quot;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>参考: https://realpython.com/python-exceptions/</p>`,19),o=[i];function p(c,l){return s(),a("div",null,o)}const d=n(t,[["render",p],["__file","exception.html.vue"]]);export{d as default};
---
+++ assets/next查看更多功能.html-ttg1k8v_.js
@@ -0,0 +1,30 @@
+import{_ as t}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as o,o as l,c as i,a as n,b as s,d as c,e as a}from"./app-0JgdiRQ-.js";const p={},r=a(`<h1 id="next查看更多功能" tabindex="-1"><a class="header-anchor" href="#next查看更多功能" aria-hidden="true">#</a> next查看更多功能</h1><h2 id="打开read-more-btn" tabindex="-1"><a class="header-anchor" href="#打开read-more-btn" aria-hidden="true">#</a> 打开read_more_btn</h2><p>用来控制当使用<code>&lt;--more--&gt;</code>生成摘要时显示的<strong>read more</strong>按钮是否显示</p><div class="language-yaml line-numbers-mode" data-ext="yml"><pre class="language-yaml"><code><span class="token key atrule">read_more_btn</span><span class="token punctuation">:</span> <span class="token boolean important">false</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p><a href="./next%E6%9F%A5%E7%9C%8B%E6%9B%B4%E5%A4%9A%E5%8A%9F%E8%83%BD/1.png"></a></p><h2 id="配置custom-file-path" tabindex="-1"><a class="header-anchor" href="#配置custom-file-path" aria-hidden="true">#</a> 配置custom_file_path</h2>`,6),d={href:"https://theme-next.js.org/docs/advanced-settings/custom-files.html",target:"_blank",rel:"noopener noreferrer"},u=a(`<div class="language-json line-numbers-mode" data-ext="json"><pre class="language-json"><code>custom_file_path<span class="token operator">:</span>
+  #head<span class="token operator">:</span> source/_data/head.njk
+  #header<span class="token operator">:</span> source/_data/header.njk
+  #sidebar<span class="token operator">:</span> source/_data/sidebar.njk
+  #postMeta<span class="token operator">:</span> source/_data/post-meta.njk
+  #postBodyEnd<span class="token operator">:</span> source/_data/post-body-end.njk
+  #footer<span class="token operator">:</span> source/_data/footer.njk
+  #bodyEnd<span class="token operator">:</span> source/_data/body-end.njk
+  #variable<span class="token operator">:</span> source/_data/variables.styl
+  #mixin<span class="token operator">:</span> source/_data/mixins.styl
+  #style<span class="token operator">:</span> source/_data/styles.styl
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ol><li>开启自定义配置文件, 在<code>next/_config.yml</code>中开启自定义配置</li></ol><div class="language-yaml line-numbers-mode" data-ext="yml"><pre class="language-yaml"><code><span class="token key atrule">custom_file_path</span><span class="token punctuation">:</span>
+  <span class="token key atrule">style</span><span class="token punctuation">:</span> source/_data/styles.styl
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><ol start="2"><li>编辑自定义样式, 在<code>hexo/source/_data/styles.styl</code>中编辑</li></ol><div class="language-css line-numbers-mode" data-ext="css"><pre class="language-css"><code><span class="token selector">.posts-expand .post-meta</span> <span class="token punctuation">{</span>
+    <span class="token property">margin</span><span class="token punctuation">:</span> 3px 0 3px 0<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">.post-block</span> <span class="token punctuation">{</span>
+    <span class="token property">padding</span><span class="token punctuation">:</span> 20px<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">p</span> <span class="token punctuation">{</span>
+    <span class="token property">margin</span><span class="token punctuation">:</span> 0 0 0 0<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">.post-body</span> <span class="token punctuation">{</span>
+    <span class="token property">font-size</span><span class="token punctuation">:</span> 1em<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token selector">body</span> <span class="token punctuation">{</span>
+    <span class="token property">line-height</span><span class="token punctuation">:</span> 1.5<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="最终效果" tabindex="-1"><a class="header-anchor" href="#最终效果" aria-hidden="true">#</a> 最终效果</h2><p>这种样式配合<code>&lt;--more--&gt;</code>摘要,可以将首页文章展示做小,而不是展示文章所有内容.</p><p><a href="./next%E6%9F%A5%E7%9C%8B%E6%9B%B4%E5%A4%9A%E5%8A%9F%E8%83%BD/2.png"></a></p>`,8);function m(v,k){const e=o("ExternalLinkIcon");return l(),i("div",null,[r,n("p",null,[s("用来配置自定义的js和样式等,详细参考"),n("a",d,[s("官网"),c(e)])]),u])}const _=t(p,[["render",m],["__file","next查看更多功能.html.vue"]]);export{_ as default};
